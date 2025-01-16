2128b213332aa7581671aa020857eab6a58e06c7
Ahmed Marzook <m4marzook@gmail.com>
2025-01-16 15:59:59 +0000

    Updated vite config CNAME

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { ghPages } from "vite-plugin-gh-pages";
 
+import fs from "node:fs";
+import path from "node:path";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [
+    react(),
+    ghPages({
+      // Add this hook to create CNAME file before publishing
+      onBeforePublish: ({ outDir }) => {
+        const CNAME = path.join(outDir, "CNAME");
+        fs.writeFileSync(CNAME, "tenzies.kaizenflow.dev");
+      },
+    })
+  ],
   base: '/',
 })
