a980d3ffc1b08e04b966fdfab9f1895a5ed8cbe0
Ahmed Marzook <m4marzook@gmail.com>
2025-01-21 19:51:03 +0000

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ function App() {
     <>
       <img src={tenziesLogo}></img>
       <GamePage />
+      <footer>
+        <div>
+          A{" "}
+          <a href="https://github.com/ahmed-marzook">KaizenFlow Technologies</a>{" "}
+          project by{" "}
+          <a href="https://github.com/ahmed-marzook/Tenzies-Learning-React">
+            Ahmed Marzook
+          </a>
+        </div>
+        <div>
+          Inspired by <a href="https://scrimba.com/@bobziroll">Bob Ziroll</a>
+        </div>
+      </footer>
     </>
   );
 }
